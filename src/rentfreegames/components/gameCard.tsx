279e04d834ec94b22f90da9da14134166550a895
James Ekstrom <james@jamesekstrom.com>
2023-04-28 11:59:24 -0700

    Dynamically load game card component for performance.

---
+++ src/rentfreegames/components/gameCard.tsx
@@ -0,0 +1,80 @@
+import { Typography } from "@mui/material";
+import Box from '@mui/material/Box';
+import { motion, color, useMotionValue, useTransform } from "framer-motion";
+import { Game } from "../interfaces";
+import { tomato } from "../styles/theme";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PeopleIcon from '@mui/icons-material/People';
+
+export default function SessionSwiping({ game, onDragEnd }: { game: Game, onDragEnd: (event: any, info: any, game: Game) => void }) {
+    const x = useMotionValue(0);
+    const xInput = [-100, 0, 100];
+    const rotate = useTransform(x, [-230, 230], [-38, 38])
+    const color = useTransform(x, xInput, [
+      "rgb(255, 0, 0)",
+      "rgb(0, 0, 255)",
+      "rgb(3, 209, 0)"
+    ]);
+    const tickPath = useTransform(x, [10, 100], [0, 1]);
+    const crossPathA = useTransform(x, [-10, -55], [0, 1]);
+    const crossPathB = useTransform(x, [-50, -100], [0, 1]);
+    
+    return (<Box sx={{ position: "absolute", width: "100%", overflow: "hidden", userSelect: "none" }} key={`box-${game.id}`}>
+        <motion.div className="container" style={{ backgroundColor: "transparent" }} key={`container-${game.id}`} >
+            <motion.div
+                className="box"
+                style={{ x, rotate, opacity: 1 }}
+                drag="x"
+                dragConstraints={{ left: 0, right: 0 }}
+                key={game.id}
+                onDragEnd={async (event, info) => await onDragEnd(event, info, game)}
+            >
+                <Box key={game.id} sx={{
+                    width: "100%",
+                    minHeight: "100%",
+                    backgroundImage: `linear-gradient(to bottom, transparent 30%, black), url("${game.image_url}");`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundColor: "transparent",
+                    position: "relative"
+                }}>
+                    <Box sx={{ position: "absolute", bottom: "5%", margin: 3, userSelect: "none" }}>
+                        <Typography sx={{ textShadow: "1px 1px 7px black" }}>{game.name}, <FavoriteIcon sx={{ fontSize: 15, color: tomato }} /> {game.avg_rating}</Typography>
+                        <Typography sx={{ fontSize: 12, textShadow: "1px 1px 7px black" }}>{game?.primary_publisher?.name ?? "unknown publisher"}</Typography>
+                        <Typography sx={{ fontSize: 12, textShadow: "1px 1px 7px black" }}><PeopleIcon sx={{ fontSize: 15, color: "secondary.main" }} />&nbsp;{game.ownedBy.map(o => o.name).join(", ")}</Typography>
+                    </Box>
+                    <svg className="progress-icon" viewBox="0 0 50 50">
+                        <motion.path
+                            fill="none"
+                            strokeWidth="2"
+                            stroke={color}
+                            key={`p2-${game.id}`}
+                            d="M14,26 L 22,33 L 35,16"
+                            strokeDasharray="0 1"
+                            style={{ pathLength: tickPath, opacity: 1 }}
+                        />
+                        <motion.path
+                            fill="none"
+                            strokeWidth="2"
+                            stroke={color}
+                            key={`p3-${game.id}`}
+                            d="M17,17 L33,33"
+                            strokeDasharray="0 1"
+                            style={{ pathLength: crossPathA, opacity: 1 }}
+                        />
+                        <motion.path
+                            fill="none"
+                            strokeWidth="2"
+                            stroke={color}
+                            key={`p4-${game.id}`}
+                            d="M33,17 L17,33"
+                            strokeDasharray="0 1"
+                            style={{ pathLength: crossPathB, opacity: 1 }}
+                        />
+                    </svg>
+                </Box>
+            </motion.div>
+        </motion.div>
+    </Box>)
+}
--- src/rentfreegames/components/sessionSwiping.tsx
+++ src/rentfreegames/components/sessionSwiping.tsx
@@ -2,7 +2,7 @@ import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
-import { AppBar, Button, IconButton, Toolbar, Typography, useMediaQuery } from '@mui/material';
+import { AppBar, Button, IconButton, Toolbar, Typography, Grid, useMediaQuery } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import * as React from 'react';
@@ -19,6 +19,8 @@ import CloseIcon from '@mui/icons-material/Close';
 import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
 import { tomato } from '../styles/theme';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import ConfettiExplosion from 'react-confetti-explosion';
+import dynamic from 'next/dynamic';
 
 const postData = async (url: string, data: any) => {
     const response = await fetch(url, {
@@ -53,7 +55,7 @@ const style = {
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-  };
+};
 
 function getUniqueGames(games: Game[]) {
     // Unique games by BGGId
@@ -77,15 +79,16 @@ export default function SessionSwiping() {
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
     const [snackState, setSnackState] = React.useState(false);
+    const [isExploding, setIsExploding] = React.useState(false);
 
-      const showSnack = () => {
+    const showSnack = () => {
         setSnackState(true);
-      };
-    
-      const handleSnackClose = () => {
+    };
+
+    const handleSnackClose = () => {
         setSnackState(false);
-      };
-    
+    };
+
     const { query } = useRouter()
     const { data, error, isLoading, isValidating } = getSession(query?.id as string, guestUser?.id)
 
@@ -130,18 +133,6 @@ export default function SessionSwiping() {
 
     const [swipableGames, setSwipableGames] = React.useState(getUserSwipableGames(data?.gameSession?.users, data.sessionUser?.id ?? guestUser.id, data?.gameSession?.userGameRatings, data?.gameSession?.userSwipes));
 
-    const x = useMotionValue(0);
-    const xInput = [-100, 0, 100];
-    const rotate = useTransform(x, [-230, 230], [-38, 38])
-    const color = useTransform(x, xInput, [
-      "rgb(255, 0, 0)",
-      "rgb(0, 0, 255)",
-      "rgb(3, 209, 0)"
-    ]);
-    const tickPath = useTransform(x, [10, 100], [0, 1]);
-    const crossPathA = useTransform(x, [-10, -55], [0, 1]);
-    const crossPathB = useTransform(x, [-50, -100], [0, 1]);
-
     if (error) {
         console.log("Failed to load");
         return <div>Failed to load</div>
@@ -162,145 +153,98 @@ export default function SessionSwiping() {
         setOpen(false);
     };
 
-    const addUserSwipedGame = async (gameId: string, liked: boolean) => {
-        if (data?.gameSession?.id && gameId) {
-            await postData(`/api/sessions/${data.gameSession.id}/user/${data.sessionUser?.id  ?? guestUser.id}`, { gameId, liked });
+    const onDragEnd = async (event, info, game) => {
+        if (info.offset.x > 200) {
+            setSwipableGames(swipableGames.filter(g => g.id !== game.id));
+            await addUserSwipedGame(game.id, true);
+        } else if (info.offset.x < -200) {
+            setSwipableGames(swipableGames.filter(g => g.id !== game.id));
+            await addUserSwipedGame(game.id, false);
+        }
+        if (swipableGames.length === 1) {
+            setOpen(false);
+            showSnack()
+            setIsExploding(true);
         }
     }
+
+
+    const addUserSwipedGame = async (gameId: string, liked: boolean) => {
+        if (data?.gameSession?.id && gameId) {
+            await postData(`/api/sessions/${data.gameSession.id}/user/${data.sessionUser?.id ?? guestUser.id}`, { gameId, liked });
+        }
+    }
+
+    const GameCard = dynamic(() => import('./gameCard'))
 
     function GameModal({ games }: { games: Game[] | null }) {
         return (
             <Modal
-            aria-labelledby={`transition-modal-games`}
-            aria-describedby={`transition-modal-games-swiper`}
-            open={open}
-            onClose={handleClose}
-            closeAfterTransition
-            slots={{ backdrop: Backdrop }}
-            sx={{ backdropFilter: "blur(5px)", }}
-            slotProps={{
-              backdrop: {
-                timeout: 500,
-              },
-            }}
-          >
-            <Fade in={open}>
-                <Box sx={{ 
-                    position: "absolute", 
-                    top: fullScreen ? "75%" : "50%", 
-                    left: "50%", 
-                    transform: 'translate(-50%, -50%)', 
-                    width: fullScreen ? "100%" : "600px", 
-                    height: fullScreen ? "100%" : "600px",
-                    bgcolor: "transparent", 
-                    color: "primary.contrastText", 
-                    overflow: "hidden" 
-                }} key="swipable-games">
-                    {
-                        games ? games.map((game) => (
-                            swipableGames.some(g => g.id == game.id)
-                            ? <GameCard game={game} key={game.id}/>
-                            : <div key={game.id}></div>
-                        ))
-                        : <Box sx={{ width: "100%", overflow: "hidden", userSelect: "none", display: "none" }}/>
-                    }
-                </Box>
-            </Fade>
-          </Modal>
+                aria-labelledby={`transition-modal-games`}
+                aria-describedby={`transition-modal-games-swiper`}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                sx={{ backdropFilter: "blur(5px)", }}
+                slotProps={{
+                    backdrop: {
+                        timeout: 500,
+                    },
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={{
+                        position: "absolute",
+                        top: fullScreen ? "75%" : "50%",
+                        left: "50%",
+                        transform: 'translate(-50%, -50%)',
+                        width: fullScreen ? "100%" : "600px",
+                        height: fullScreen ? "100%" : "600px",
+                        bgcolor: "transparent",
+                        color: "primary.contrastText",
+                        overflow: "hidden"
+                    }} key="swipable-games">
+                        {
+                            games ? games.map((game) => (
+                                swipableGames.some(g => g.id == game.id)
+                                    ? <GameCard game={game} onDragEnd={onDragEnd} key={game.id} />
+                                    : <div key={game.id}></div>
+                            ))
+                                : <Box sx={{ width: "100%", overflow: "hidden", userSelect: "none", display: "none" }} />
+                        }
+                    </Box>
+                </Fade>
+            </Modal>
         );
     }
 
-    function GameCard({ game }:{ game: Game }) {
-        return (<Box sx={{ position: "absolute", width: "100%", overflow: "hidden", userSelect: "none" }} key={`box-${game.id}`}>
-                <motion.div className="container" style={{ backgroundColor: "transparent" }} key={`container-${game.id}`} >
-                    <motion.div
-                        className="box"
-                        style={{ x, rotate, opacity: 1 }}
-                        drag="x"
-                        dragConstraints={{ left: 0, right: 0 }}
-                        key={game.id}
-                        onDragEnd={async (event, info) => {
-                            if (info.offset.x > 200) {
-                                setSwipableGames(swipableGames.filter(g => g.id !== game.id));
-                                await addUserSwipedGame(game.id, true);
-                            } else if (info.offset.x < -200) {
-                                setSwipableGames(swipableGames.filter(g => g.id !== game.id));
-                                await addUserSwipedGame(game.id, false);
-                            }
-                            if (swipableGames.length === 1) {
-                                setOpen(false);
-                                showSnack()
-                            }
-                        }}
-                    >
-                        <Box key={game.id} sx={{ 
-                                width: "100%", 
-                                minHeight: "100%",
-                                backgroundImage: `linear-gradient(to bottom, transparent 30%, black), url("${game.image_url}");`, 
-                                backgroundSize: "cover", 
-                                backgroundRepeat: "no-repeat",
-                                backgroundPosition: "center",
-                                backgroundColor: "transparent",
-                                position: "relative"
-                            }}>
-                            <Box sx={{ position: "absolute", bottom: "5%", margin: 3, userSelect: "none" }}>
-                                <Typography sx={{ textShadow: "1px 1px 7px black" }}>{game.name}, <FavoriteIcon sx={{ fontSize: 15, color: tomato }}/> {game.avg_rating}</Typography>
-                                <Typography sx={{ fontSize: 12, textShadow: "1px 1px 7px black" }}>{game?.primary_publisher?.name ?? "unknown publisher"}</Typography>
-                                <Typography sx={{ fontSize: 12, textShadow: "1px 1px 7px black" }}><PeopleIcon sx={{ fontSize: 15, color: "secondary.main" }}/>&nbsp;{game.ownedBy.map(o => o.name).join(", ")}</Typography>
-                            </Box>
-                            <svg className="progress-icon" viewBox="0 0 50 50">
-                                <motion.path
-                                    fill="none"
-                                    strokeWidth="2"
-                                    stroke={color}
-                                    key={`p2-${game.id}`}
-                                    d="M14,26 L 22,33 L 35,16"
-                                    strokeDasharray="0 1"
-                                    style={{ pathLength: tickPath, opacity: 1 }}
-                                />
-                                <motion.path
-                                    fill="none"
-                                    strokeWidth="2"
-                                    stroke={color}
-                                    key={`p3-${game.id}`}
-                                    d="M17,17 L33,33"
-                                    strokeDasharray="0 1"
-                                    style={{ pathLength: crossPathA, opacity: 1 }}
-                                />
-                                <motion.path
-                                    fill="none"
-                                    strokeWidth="2"
-                                    stroke={color}
-                                    key={`p4-${game.id}`}
-                                    d="M33,17 L17,33"
-                                    strokeDasharray="0 1"
-                                    style={{ pathLength: crossPathB, opacity: 1 }}
-                                />
-                            </svg>
-                        </Box>
-                    </motion.div>
-                </motion.div>
-        </Box>)
-    }
-
     return (
-        <Box sx={{ paddingTop: 1, paddingRight: 1 }}>
-            {
-                swipableGames.length > 0 
-                ? <Button variant="contained" onClick={handleClickOpen} sx={{ width: "100%", bgcolor: "secondary.main", color: "secondary.contrastText" }}>
-                        Start Swiping
-                    </Button>
-                : <></>
-            }
-            
-            <GameModal games={getUserGames(data?.gameSession?.users, data.sessionUser?.id ?? guestUser.id, data?.gameSession?.userGameRatings)}/>
-            <Snackbar
-                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-                open={snackState}
-                onClose={handleSnackClose}
-                message="Swiping complete!"
-                key="bottomcenter"
-            />
-        </Box>
+        <Grid container>
+            <Grid item xs={12} sm={12} md={12} sx={{ display: "flex", justifyContent: "center", alignContent: "center" }}>
+                <>{isExploding && <ConfettiExplosion particleCount={250} />}</>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+                <Box sx={{ paddingTop: 1, paddingRight: 1 }}>
+
+                    {
+                        swipableGames.length > 0
+                            ? <Button variant="contained" onClick={handleClickOpen} sx={{ width: "100%", bgcolor: "secondary.main", color: "secondary.contrastText" }}>
+                                Start Swiping
+                            </Button>
+                            : <></>
+                    }
+
+                    <GameModal games={getUserGames(data?.gameSession?.users, data.sessionUser?.id ?? guestUser.id, data?.gameSession?.userGameRatings)} />
+                    <Snackbar
+                        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                        open={snackState}
+                        onClose={handleSnackClose}
+                        message="Swiping complete!"
+                        key="bottomcenter"
+                    />
+                </Box>
+            </Grid>
+        </Grid>
     );
 }
